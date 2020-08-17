be50a31c0f6a3ae2c3e1b8eb96d454a4ff185f27
vchantreauj <vchantreau@free.fr>
2020-08-17 10:46:40 +0200

    pep8 optim

--- unet_main.py
+++ unet_main.py
@@ -2,46 +2,22 @@
 
 import time
 from datetime import date
-from skimage import io
 import numpy as np
 from tqdm import tqdm
-from torchvision.utils import save_image
 import torch
 import unet_class
 # exec(open("unet_main.py").read())
 
-
-def load_train_set(repim, repmask, width, height, crop_size, nb_im):
-    """load images and get the train set
-    width and height are required for the image to support the successives
-    convolutions
-    """
-    print(repim)
-    images = io.imread_collection(repim, plugin='tifffile')
-    print(len(images))
-    masks = io.imread_collection(repmask, plugin='tifffile')
-
-    x_train = []
-    labels = []
-    for i in range(nb_im):
-        x_train.append(images[i][:width, :height])
-        labels.append(masks[i][crop_size:width - crop_size,
-                               crop_size:height - crop_size])
-
-    x_train = np.array(x_train) / 255
-    x_train = np.transpose(x_train, (0, 3, 1, 2))
-    labels = np.array(labels) / 255
-    return x_train, labels
-
-
+#global UNET
+UNET = unet_class.UNet(in_channel=3, out_channel=2)
+UNET = UNET.cuda()
 def train_step(inputs, labels, optimizer, criterion, batch_size, crop_size):
     """training part"""
-    global unet
     width_out = inputs.shape[2] - 2 * crop_size
     height_out = inputs.shape[3] - 2 * crop_size
     optimizer.zero_grad()
     # forward + backward + optimize
-    outputs = unet(inputs)
+    outputs = UNET(inputs)
     outputs = outputs.permute(0, 2, 3, 1)
     outputs = outputs.resize(batch_size * width_out * height_out, 2)
     labels = labels.resize(batch_size * width_out * height_out)
@@ -63,11 +39,10 @@ def log_unet(epochs, nb_im, total_loss, batch_loss, time_unet):
 
 def train_unet(batch_size, epochs, x_train, y_train, crop_size):
     """loop to train the unet model"""
-    global unet
-    unet = unet_class.UNet(in_channel=3, out_channel=2)
-    unet = unet.cuda()
+    #UNET = unet_class.UNet(in_channel=3, out_channel=2)
+    #UNET = UNET.cuda()
     criterion = torch.nn.CrossEntropyLoss()
-    optimizer = torch.optim.SGD(unet.parameters(), lr=0.01, momentum=0.99)
+    optimizer = torch.optim.SGD(UNET.parameters(), lr=0.01, momentum=0.99)
     nb_im = len(x_train)
     epoch_iter = np.ceil(nb_im / batch_size).astype(int)
     time0 = time.time()
@@ -91,7 +66,7 @@ def train_unet(batch_size, epochs, x_train, y_train, crop_size):
     time1 = time.time()
     today = date.today()
     unetfile = today.strftime("%y%m%d") + 'unet' + str(epochs) + 'ep.pt'
-    torch.save(unet.state_dict(), unetfile)
+    torch.save(UNET.state_dict(), unetfile)
     log_unet(epochs, nb_im, total_loss, batch_loss, time1 - time0)
 
 
@@ -106,12 +81,13 @@ def main():
     crop_size = 44
     nb_epochs = 3
 
-    x_train, y_train = load_train_set(repim,
-                                      repmask,
-                                      width=764,
-                                      height=892,
-                                      crop_size=crop_size,
-                                      nb_im=nb_im)
+    x_train, y_train = unet_class.load_set(
+        repim,
+        repmask,
+        width=764,
+        height=892,
+        crop_size=crop_size,
+        nb_im=nb_im)
 
     train_unet(batch_size=1, epochs=nb_epochs, x_train=x_train,
                y_train=y_train, crop_size=crop_size)
